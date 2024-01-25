5f191edf6f04f6910fd0440bf67c9ecf05e4d7e0
AlbinLind <albin.lindqvist@hotmail.se>
2024-01-25 16:49:03 +0100

    Add python configuration

---
+++ lua/plugins/python.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+    cmd = "VenvSelector",
+    opts = function(_, opts)
+      if require("lazyvim.util").has("nvim-dap-python") then
+        opts.dap_enabled = true
+      end
+      return vim.tbl_deep_extend("force", opts, {
+        name = {
+          "venv", ".venv", "env", ".env"
+        },
+      })
+    end,
+    keys = { { "<leader>cvs", "<cmd>:VenvSelect<cr>", desc = "Select Virtual Environmet" }, { "<leader>cvc", "<cmd>:VenvSelectCached<cr>", desc = "Use Latest Virtual Environment" } },
+  },
+  {
+    "wookayin/semshi",
+    ft = "python",
+    build = ":UpdateRemotePlugins",
+    init = function()
+      -- Disabled these features better provided by LSP or other more general plugins
+      vim.g["semshi#error_sign"] = false
+      vim.g["semshi#simplify_markup"] = false
+      vim.g["semshi#mark_selected_nodes"] = false
+      vim.g["semshi#update_delay_factor"] = 0.001
+
+      -- This autocmd must be defined in init to take effect
+      vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
+        group = vim.api.nvim_create_augroup("SemanticHighlight", {}),
+        callback = function()
+          -- Only add style, inherit or link to the LSP's colors
+          vim.cmd([[
+            highlight! link semshiImported @lsp.type.namespace
+            highlight! link semshiParameter @lsp.type.parameter
+            highlight! link semshiParameterUnused DiagnosticUnnecessary
+            highlight! link semshiBuiltin @function.builtin
+            highlight! link semshiAttribute @field
+            highlight! link semshiSelf @lsp.type.selfKeyword
+            highlight! link semshiUnresolved @lsp.type.unresolvedReference
+            highlight! link semshiFree @comment
+            ]])
+        end,
+      })
+    end,
+  }
+}
