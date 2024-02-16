c80e6bde49be44c72dc0f64fcfe827ea28349424
AlbinLind <albin.lindqvist@hotmail.se>
2024-02-16 17:46:37 +0100

    Python config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,10 +3,10 @@
 -- Add any additional autocmds here
 --
 -- Run :VenvSelectCached when entering a python file
---vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  --callback = function()
-    --require("venv-selector").retrieve_from_cache()
-    -- Reload the current buffer to activate the new venv
-  --end,
-  --once = true
-  --})
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	callback = function()
+		require("venv-selector").retrieve_from_cache()
+		-- Reload the current buffer to activate the new venv
+	end,
+	once = true,
+})
