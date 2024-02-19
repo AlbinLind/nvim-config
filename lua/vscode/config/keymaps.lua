a1c356ac03dce8492fbcabdc9b12665e0fcf475a
Albin Lindqvist <albin.lindqvist@hotmail.se>
2024-02-19 11:03:36 +0100

    Update keymaps.lua with new VSCode shortcuts

--- lua/vscode/config/keymaps.lua
+++ lua/vscode/config/keymaps.lua
@@ -14,14 +14,13 @@ map("n", "<leader>ff", function() vscode.action("workbench.action.findInFiles") 
 -- VSCode open command pallet
 map("n", "<leader>cp", function() vscode.action("workbench.action.showCommands") end, { desc = "Open command pallet" })
 
--- VSCode open file explorer
-map("n", "<leader>fe", function() vscode.action("workbench.view.explorer") end, { desc = "Open file explorer" })
-
--- VSCode open source control
+-- VSCode open certain views
+map("n", "<leader>e", function() vscode.action("workbench.view.explorer") end, { desc = "Open file explorer" })
 map("n", "<leader>g", function() vscode.action("workbench.view.scm") end, { desc = "Open source control" })
-
--- VSCode open test explorer
-map("n", "<leader>ft", function() vscode.action("workbench.view.extension.test") end, { desc = "Open test explorer" })
+map("n", "<leader>ct", function() vscode.action("workbench.view.extension.test") end, { desc = "Open test explorer" })
+map("n", "<leader>cd", function() vscode.action("workbench.view.debug") end, { desc = "Open debug" })
+map("n", "<leader>t", function() vscode.action("workbench.action.terminal.toggleTerminal") end, { desc = "Open terminal" }) 
+map("n", "<leader>sv", function() vscode.action("workbench.action.toggleSidebarVisibility") end, { desc = "Toggle sidebar" })
 
 -- Remap :s to VSCode search and replace
 map("n", "<leader>sr", function() vscode.action("editor.action.startFindReplaceAction") end, { desc = "Search and replace" })
@@ -32,6 +31,16 @@ map("n", "<C-u>", "<C-u>zz")
 
 -- Open "file tree"
 map({ "n", "v" }, "<leader><leader>", vim.cmd.Ex, { desc = "Search for file" })
+
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Right>", function() vscode.action("workbench.action.increaseViewSize") end, { desc = "Increase window height" })
+map("n", "<C-Left>", function() vscode.action("workbench.action.decreaseViewSize") end, { desc = "Decrease window height" })
 
 -- Move Lines
 map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
