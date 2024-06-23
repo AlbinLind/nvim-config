a3bb9c49b7ac7959701397508bfa7a5f9d203c11
Albin Lindqvist <albin.lindqvist@hotmail.se>
2024-06-23 13:57:21 +0200

    Makes it compatible with nixos

--- init.lua
+++ init.lua
@@ -1,4 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+local f = io.open("/home/albin/.config/tree_config.lua", "r")
+if f ~= nil then
+  package.path = package.path .. ";/home/albin/.config/tree_config.lua"
+  require("tree_config")
+end
 require("config.set")
 require("config.keymaps")
 require("config.lazy")
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,48 @@
+-- From https://gist.github.com/Zbizu/43df621b3cd0dc460a76f7fe5aa87f30
+function getOS()
+	-- Unix, Linux variants
+	local fh,err = assert(io.popen("uname -o 2>/dev/null","r"))
+	if fh then
+    local f, err = assert(io.open("/etc/lsb-release", "r"))
+    if f then
+      os_info = f:read("*a")
+      local is_nix = string.find(os_info, "NixOS")
+      if is_nix then
+        return "NixOS"
+      else
+        return "Linux"
+      end
+    else
+      osname = fh:read()
+    end
+	end
+
+	return osname or "Windows"
+end
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
+    build = function() 
+      if getOS() == "NixOS" then
+        return ""
+      end
+      return ":TSUpdate"
+    end,
+    config = function ()
       local configs = require("nvim-treesitter.configs")
 
+      local to_install = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" }
+
+      -- if getOS() == "NixOS" then
+      --   to_install = {}
+      -- end
+
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          ensure_installed = to_install,
           sync_install = false,
           highlight = { enable = true },
-          indent = { enable = true },  
+          indent = { enable = true },
         })
     end
   }
