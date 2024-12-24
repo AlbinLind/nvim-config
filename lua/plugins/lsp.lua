aaaa4d7e533878f5816f42b2c996ca8449a588f4
Albin Lindqvist <albin.lindqvist@hotmail.se>
2024-12-24 12:18:53 +0100

    update lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,122 +1,116 @@
-return {
-  -- Add rustaceanvim
-  { 'mrcjkb/rustaceanvim', version = "^4", lazy = false },
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    lazy = true,
-    config = false,
-    init = function()
-      -- Disable automatic setup, we are doing it manually
-      vim.g.lsp_zero_extend_cmp = 0
-      vim.g.lsp_zero_extend_lspconfig = 0
-    end,
-  },
-  {
-    'williamboman/mason.nvim',
-    lazy = false,
-    config = true,
-  },
-  -- Autocompletion
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      { 'L3MON4D3/LuaSnip' },
+return { {
+  'saghen/blink.cmp',
+  -- use a release tag to download pre-built binaries
+  version = '0.8.*',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    -- See the full "keymap" documentation for information on defining your own keymap.
+    keymap = {
+      preset = "default",
+      ["<C-j>"] = { "select_and_accept", "accept" },
+      ["<C-space>"] = { "show" },
+
     },
-    -- window = {
-    --   vim.
-    -- },
-    config = function()
-      -- Here is where you configure the autocompletion settings.
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_cmp()
 
-      -- And you can configure cmp even more, if you want to.
-      local cmp = require('cmp')
-      local cmp_action = lsp_zero.cmp_action()
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- Will be removed in a future release
+      use_nvim_cmp_as_default = false,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
 
-      cmp.setup({
-        formatting = lsp_zero.cmp_format({ details = true }),
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-          ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<C-j>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<C-CR>"] = function(fallback)
-            cmp.abort()
-            fallback()
-          end,
-        }),
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
-      })
-    end
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
   },
+  opts_extend = { "sources.default" }
+},
+  -- LSP servers and clients communicate which features they support through "capabilities".
+  --  By default, Neovim supports a subset of the LSP specification.
+  --  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
+  --  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
+  --
+  -- This can vary by config, but in general for nvim-lspconfig:
 
-  -- LSP
   {
     'neovim/nvim-lspconfig',
-    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-    event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = {
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'williamboman/mason-lspconfig.nvim' },
+    dependencies = { 'saghen/blink.cmp',
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "stevearc/conform.nvim"
     },
-    config = function()
-      -- This is where all the LSP shenanigans will live
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_lspconfig()
-
-      --- if you want to know more about lsp-zero and mason.nvim
-      --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-      lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        lsp_zero.default_keymaps({ buffer = bufnr })
-      end)
-      vim.g.rustaceanvim = {
-        server = {
-          capabilities = lsp_zero.get_capabilities()
-        }
+    opts = {
+      servers = {
+        lua_ls = {},
       }
-      require('mason-lspconfig').setup({
-        ensure_installed = {},
-        handlers = {
-          -- this first function is the "default handler"
-          -- it applies to every language server without a "custom handler"
-          function(server_name)
-            require('lspconfig')[server_name].setup({})
-          end,
-          -- Do not use rust_analyzer
-          rust_analyzer = lsp_zero.noop,
-          -- this is the "custom handler" for `lua_ls`
-          lua_ls = function()
-            -- (Optional) Configure lua language server for neovim
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-          end,
-          pylsp = function()
-            require('lspconfig').pylsp.setup({
-              settings = {
-                pylsp = {
-                  plugins = {
-                    pycodestyle = {
-                      maxLineLength = 120
-                    },
-                  }
-                }
-              }
-            })
-          end
-        }
+    },
+    config = function(_, opts)
+      require('mason').setup({})
+      require('mason-lspconfig').setup({})
+
+      local lspconfig = require('lspconfig')
+
+      for server, config in pairs(opts.servers) do
+        -- passing config.capabilities to blink.cmp merges with the capabilities in your
+        -- `opts[server].capabilities, if you've defined it
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+
+      -- Add custom keymap for ruff
+      lspconfig["ruff"].setup({
+        on_attach = function(client, bufnr)
+          vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>co',
+            '<cmd>lua vim.lsp.buf.code_action({apply = true,context = {only = {"source.organizeImports"},diagnostics = {},}})<cr>',
+            { desc = "Organize Imports", noremap = true, silent = true })
+        end
       })
     end
+  },
+  {
+    "mason.nvim",
+    {
+      "williamboman/mason-lspconfig.nvim",
+      lazy = false,
+      config = function() end,
+      opts = {
+        ensure_installed = {
+          -- "lua_ls",
+          "basedpyright",
+          "ruff-lsp",
+          "rust_analyzer",
+          "marksman",
+          "harper_ls",
+          "taplo",
+          "stylua",
+        },
+      }
+    }
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      default_format_opts = {
+        lsp_format = "fallback"
+      },
+      formatters_by_ft = {
+        python = {
+          "ruff"
+        },
+        lua = {
+          "stylua"
+        }
+      }
+    }
   }
 }
