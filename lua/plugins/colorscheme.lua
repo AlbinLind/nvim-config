4239414c3da310aaf6b1b4c4602bc8de12065d46
Albin Lindqvist <albin.lindqvist@hotmail.se>
2025-03-06 20:46:52 +0100

    format?

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,19 @@
 return {
-  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = {
-    italic = {
-      strings = false,
-      emphasis = false,
-      comments = false,
-      operators = false,
-      folds = false
-    }
-  },
-  init = function ()
-    vim.cmd("colorscheme gruvbox") 
-  end
-}  
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      italic = {
+        strings = false,
+        emphasis = false,
+        comments = false,
+        operators = false,
+        folds = false
+      }
+    },
+    init = function()
+      vim.cmd("colorscheme gruvbox")
+    end
+  }
 }
