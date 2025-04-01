c32eac85652fd13f442676a04a0fc62080500454
Albin Lindqvist <albin.lindqvist@hotmail.se>
2025-04-01 21:41:37 +0200

    use sioyek as pdf reader

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -5,6 +5,7 @@ return {
     config = function()
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_compiler_method = "tectonic"
+      vim.g.vimtex_view_method = 'sioyek'
     end,
     keys = {
       { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
