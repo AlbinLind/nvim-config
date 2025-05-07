67dd34ba1794a82358c7505b98ac369f65ab0193
Albin Lindqvist <albin.lindqvist@hotmail.se>
2025-05-07 16:31:29 +0200

    update latex config

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -6,7 +6,9 @@ return {
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_compiler_method = "tectonic"
       vim.g.vimtex_view_method = 'sioyek'
-      vim.g.vimtex_quickfix_mode = 1
+      vim.g.vimtex_quickfix_mode = 2
+      vim.g.vimtex_quickfix_autoclose_after_keystrokes = 2
+      vim.g.vimtex_quickfix_open_on_warning = 0
     end,
     keys = {
       { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
